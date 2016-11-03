f54798a27de354c44773d256996a0fc8c52079c4
Tom Beckmann <tomjonabc@gmail.com>
2016-11-03 16:37:38 +0100

    Reorder some content on the contact page

--- page-kontakt.php
+++ page-kontakt.php
@@ -14,20 +14,24 @@ $posts = get_posts(array(
 ));
 $i = 0;
 foreach ($posts as $post) {
-  $thumb = '<div class="col-sm-3">'.get_the_post_thumbnail($post, 'large', array('class' => 'circle-image')).'</div>';
+  $thumb = '<div class="col-sm-3">'.
+    '<a href="'.get_the_permalink($post).'">'.
+    get_the_post_thumbnail($post, 'large', array('class' => 'circle-image')).
+    '</a>'.
+  '</div>';
 ?>
   <div class="contact-card row">
 	<?php if ($i % 2 == 0) echo $thumb; ?>
 	<div class="col-sm-4">
-	  <h3>Wer bin ich?</h3>
-	  <?= get_the_title($post) ?> - <?php the_field('function', $post) ?><br>
+	  <h3><?= get_the_title($post) ?> </h3>
+	  <?php the_field('function', $post) ?><br>
 	  <?php the_field('address', $post) ?>
 	</div>
 	<div class="col-sm-5">
 	  <h3>Ich bin Ansprechpartner für ...</h3>
 	  <?php the_field('tasks', $post) ?>
-	  <div class="text-right">
-	    <a class="contact-more" href="<?php the_permalink() ?>">Mehr über mich ...</a>
+	  <div style="margin-top: 24px">
+	    <a class="contact-more" href="<?php get_the_permalink($post) ?>">Mehr über mich ...</a>
 	  </div>
 	</div>
 	<?php if ($i % 2 == 1) echo $thumb; ?>
