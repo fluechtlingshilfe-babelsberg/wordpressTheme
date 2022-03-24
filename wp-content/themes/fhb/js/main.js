49bc86e3329f24ec1f725c947b6f3a7749d6af36
Uta Krüger <utakrueger@gmx.net>
2022-03-24 22:38:34 +0100

    cleaned up code

--- wp-content/themes/fhb/js/main.js
+++ wp-content/themes/fhb/js/main.js
@@ -1,24 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-	if(window.location.hash && document.querySelector('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]')) {
-		console.log('hash: ' + window.location.hash);
-		var test = document.querySelector('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]');
-		var tabs = document.querySelector('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]').closest('.tabs-container');
-		tabs.querySelector('.tabs-nav a.active').removeAttribute('class');
-		tabs.querySelector('div.tab.active').classList.remove('active');
-		tabs.querySelector('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]').classList.add('active');
-		tabs.querySelector('div'+decodeURI(window.location.hash)+'.tab').classList.add('active');
-	}
-	document.querySelectorAll('.tabs-nav a, a.tab-url').forEach(function(item) {
-		item.addEventListener('click', function(event) {
-			event.preventDefault();
-			this.closest('.tabs-container').querySelector('.tabs-nav a.active').removeAttribute('class');
-			this.closest('.tabs-container').querySelector('div.tab.active').classList.remove('active');
-			this.closest('.tabs-container').querySelector('.tabs-nav a[href="'+this.getAttribute('href')+'"]').classList.add('active');
-			this.closest('.tabs-container').querySelector('div'+this.getAttribute('href')+'.tab').classList.add('active');
-		});
-	});
-}); */
-
 jQuery(document).ready(function($) {
     if(window.location.hash && $('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]').length) {
         var tabs = $('.tabs-nav a[href="'+decodeURI(window.location.hash)+'"]').parents('.tabs-container');
