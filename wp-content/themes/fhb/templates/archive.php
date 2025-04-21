073ce0e1d155820d58b610fec1ca8028584af83f
Tom Beckmann <tomjonabc@gmail.com>
2025-04-21 15:38:09 +0200

    fix archive

--- wp-content/themes/fhb/templates/archive.php
+++ wp-content/themes/fhb/templates/archive.php
@@ -2,12 +2,14 @@
 /*
  * Template Name: Archive
  */
+$current = isset($_GET['jahr']) ? $_GET['jahr'] : '2025';
+
 function get_posts_years_array() {
     global $wpdb;
     $result = array();
     $years = $wpdb->get_results(
         $wpdb->prepare(
-            "SELECT COUNT(*), YEAR(post_date) FROM {$wpdb->posts} WHERE post_status = 'publish' GROUP BY YEAR(post_date) DESC"
+            "SELECT COUNT(*), YEAR(post_date) FROM {$wpdb->posts} WHERE post_type = 'post' AND post_status = 'publish' GROUP BY YEAR(post_date) DESC"
         ),
         ARRAY_N
     );
@@ -25,15 +27,20 @@ the_post();
 <div>
     <h1><?php the_title() ?></h1>
     <?php the_content() ?>
-  <div class="uk-child-width-1-3@m" uk-grid>
-      <div>
-          <?php for ($year as $years) { ?>
-          <a href="?year=<?php echo $year[1] ?>"><?php echo $year[1] ?> (<?php echo $year[0] ?> Post(s))</a>
+      <div style="margin-bottom: 3rem">
+	Archiv für das Jahr:
+          <?php $first = true; foreach (get_posts_years_array() as $year) { ?>
+<?php
+if (!$first) {echo '&bullet;'; }
+if ($first) { $first = false; } ?>
+		  <a style="<?php echo $current == $year["year"] ? "font-weight: bold" : ""?>" href="<?php echo add_query_arg('jahr', $year['year']) ?>"><?php echo $year['year'] ?>
+		<small>(<?php echo $year['count'] ?> Post<?php echo $year['count'] == 1 ? '' : 's' ?>)</small></a>
           <?php } ?>
       </div>
+  <div class="uk-child-width-1-3@m" uk-grid>
 	<?php
 // "category__not_in" => array(17), 
-$query = new WP_Query(array("orderby" => "date", "order" => "DESC", "posts_per_page" => 12, "post_type" => "post", "post_status" => "publish", "posts_per_page" => -1));
+$query = new WP_Query(array('date_query' => array( array( 'year' => $current,),),"orderby" => "date", "order" => "DESC", "posts_per_page" => 12, "post_type" => "post", "post_status" => "publish", "posts_per_page" => -1));
 while($query->have_posts()): $query->the_post(); ?>
 		<div>
 			<?php get_template_part('templates/activity'); ?>
@@ -63,3 +70,4 @@ while($query->have_posts()): $query->the_post(); ?>
 </section>
 
 <?php get_footer(); ?>
+
